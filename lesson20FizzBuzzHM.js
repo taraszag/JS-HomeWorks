9cd50a33f81f76f7c35e3a712ddb16356de583a2
taraszag <taras288200>
2022-06-03 21:36:49 +0100

    add FizzBuzz

--- lesson20FizzBuzzHM.js
+++ lesson20FizzBuzzHM.js
@@ -1,8 +1,11 @@
-function num(num) {
-    if (num % 3 == 0) {
+function nums(num) {
+    if (num % 3 == 0 && num % 5 != 0 ) {
         return "Fizz"
     }
-    else if (num % 5 == 0){
+    else if (num % 5 == 0 && num % 3 != 0 ) {
         return "Buzz"
+    }
+    else if (num % 3 == 0 && num % 5 == 0) {
+        return "FizzBuzz"
     }
 }
--- spec/lesson20FizzBuzz_spec.js
+++ spec/lesson20FizzBuzz_spec.js
@@ -1,12 +1,16 @@
 describe("FizzBuzz", function () {
     it("number is a multiple of 3 returns “Fizz”", function () {
-        const result = num(6);
+        const result = nums(12);
 
         expect(result).to.equal("Fizz");
     });
-    it("number is a multiple of 5 return “Buzz”",function() {
-        const result = num(20);
-        
+    it("number is a multiple of 5 return “Buzz”", function () {
+        const result = nums(20);
+
         expect(result).to.equal("Buzz");
+    })
+    it("number is a multiple of both 3 and 5 return “FizzBuzz”", function () {
+        const result = nums(15);
+        expect(result).to.equal("FizzBuzz");
     })
 })
