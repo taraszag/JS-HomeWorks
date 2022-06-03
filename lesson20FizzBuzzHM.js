709c15ff665f654ac38f993c35aa1ca668d392d7
taraszag <taras288200>
2022-06-03 01:01:08 +0100

    add return Buzz

--- lesson20FizzBuzzHM.js
+++ lesson20FizzBuzzHM.js
@@ -2,4 +2,7 @@ function num(num) {
     if (num % 3 == 0) {
         return "Fizz"
     }
+    else if (num % 5 == 0){
+        return "Buzz"
+    }
 }
--- spec/lesson20FizzBuzz_spec.js
+++ spec/lesson20FizzBuzz_spec.js
@@ -1,7 +1,12 @@
 describe("FizzBuzz", function () {
-    it ("number is a multiple of 3 returns “Fizz”", function () {
+    it("number is a multiple of 3 returns “Fizz”", function () {
         const result = num(6);
 
         expect(result).to.equal("Fizz");
     });
+    it("number is a multiple of 5 return “Buzz”",function() {
+        const result = num(20);
+        
+        expect(result).to.equal("Buzz");
+    })
 })
