e195549dd182cf9d44f71014b1ee312616a4b11c
TarasZag <novotel2006@ya.ru>
2022-06-28 22:41:13 +0100

    add HW27 css+html

--- lesson26_shooters.js
+++ lesson26_shooters.js
@@ -112,3 +112,89 @@ shooters[4]();
 // for (let i =0; i < shooters.length; i += 1) {
 //   shooters[i]();
 // }
+
+{
+  'use strict';
+{
+  function makeArmy() {
+    let i = 0;
+
+    const shooters = [];
+
+    do {
+      const num = i;
+      const shooter = function () {
+        const randNum = Math.random();
+        console.log(`I'm a shooter #${num}, my random number is: ${randNum}`);
+      }
+
+      shooters.push(shooter);
+      i += 1;
+    } while (i < 10);
+
+    return shooters;
+  }
+
+  const shooters = makeArmy();
+
+  shooters[0]();
+  shooters[4]();
+
+}
+{
+  function makeArmy() {
+    let i = 0;
+
+    const shooters = [];
+
+    function makeShooter(i) {
+      return function () {
+        const randNum = Math.random();
+        console.log(`I'm a shooter #${i}, my random number is: ${randNum}`);
+      }
+    }
+    do {
+      const shooter = makeShooter(i);
+
+      shooters.push(shooter);
+      i += 1;
+    } while (i < 10);
+
+    return shooters;
+  }
+
+  const shooters = makeArmy();
+
+  shooters[0]();
+  shooters[4]();
+
+}
+{
+  function makeArmy() {
+    const shooters = [];
+
+    for(let i =0; i < 10; i += 1) {
+      const shooter = function () {
+        const randNum = Math.random();
+        console.log(`I'm a shooter #${i}, my random number is: ${randNum}`);
+      }
+
+      shooters.push(shooter);
+    } 
+
+    return shooters;
+  }
+
+  const shooters = makeArmy();
+
+  shooters[0]();
+  shooters[4]();
+}
+
+function sayHelloTo(name = "John Doe") { // let name = "John Doe"; name = "Peter";
+  console.log(`Hello ${name}`);
+}
+
+sayHelloTo("Peter");
+sayHelloTo();
+}
